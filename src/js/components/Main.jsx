a2e8c810f63fc304281192d0bb38107abf03cb7b
Shane Goodson <shanegoodson101@gmail.com>
2019-04-17 13:52:20 +0300

    Made main a functional component

--- src/js/components/Main.jsx
+++ src/js/components/Main.jsx
@@ -1,39 +1,35 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
 import Input from './TestInput.jsx';
 
-export default class Main extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: ""
-		};
-		this.handleChange = this.handleChange.bind(this);
+import '../../css/main.sass';
+
+const Main = props => {
+	const [title, setTitle] = useState("");
+
+	function handleChange(event) {
+		setTitle(event.target.value);
 	}
 
-	handleChange(event) {
-		this.setState({ [event.target.id]: event.target.value });
-	}
+	return (
+		<Fragment>
+			<div>{title}</div>
+			<form id="article-form">
+				<Input
+					text="Title"
+					label="Title"
+					type="text"
+					id="title"
+					value={title}
+					handleChange={handleChange}
+				/>
+			</form>
+		</Fragment>
+	);
+};
 
-	render() {
-		return (
-			<Fragment>
-				<div>{this.state.title}</div>
-				<form id="article-form">
-					<Input
-						text="Title"
-						label="Title"
-						type="text"
-						id="title"
-						value={this.state.title}
-						handleChange={this.handleChange}
-					/>
-				</form>
-			</Fragment>
-		);
-	}
-}
+export default Main;
 
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<Main />, wrapper) : false;
