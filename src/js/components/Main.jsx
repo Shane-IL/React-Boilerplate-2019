5f6dce7722f26556fc78f0c8acc0afb2a875fa7c
Shane Goodson <shanegoodson101@gmail.com>
2019-04-17 14:15:41 +0300

    Update Main.jsx

--- src/js/components/Main.jsx
+++ src/js/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
