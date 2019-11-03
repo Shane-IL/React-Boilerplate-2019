bac82871957b5413572ebc747ba3d9ad4bdce3eb
Shane Goodson <shanegoodson101@gmail.com>
2019-11-03 10:18:18 +0200

    Add files via upload

---
+++ src/js/components/Home/index.jsx
@@ -0,0 +1,28 @@
+import React, {Fragment, useState} from "react";
+import Input from '../Input/index';
+
+const Home = props => {
+	const [title, setTitle] = useState("");
+
+	function handleChange(event) {
+		setTitle(event.target.value);
+	}
+
+	return (
+		<Fragment>
+			<div>{title}</div>
+			<form id="article-form">
+				<Input
+					text="Title"
+					label="Title"
+					type="text"
+					id="title"
+					value={title}
+					handleChange={handleChange}
+				/>
+			</form>
+		</Fragment>
+	);
+};
+
+export default Home;
